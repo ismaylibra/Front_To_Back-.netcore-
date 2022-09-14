6bd52def73a6a5d0e72e0dab9d7e8ef90a8d1ac3
abbasoov <ismayiliib@code.edu.az>
2022-09-14 07:40:41 +0400

    created sql tables

--- RentACar/Controllers/HomeController.cs
+++ RentACar/Controllers/HomeController.cs
@@ -14,64 +14,21 @@ namespace RentACar.Controllers
         public HomeController(AppDbContext context)
         {
             _context = context;
-
-            Cars = new List<Car>
-            {
-                new Car {
-                    Id = 1,
-                    Name ="BMW X5",
-                    Price = 99,
-                    GearBox= "Automatic",
-                    Mileage = 79,
-                    Year = 2015,
-                   
-                    CarImages = new List<CarImage> {
-                        new CarImage {Id = 1, ImageUrl = "car-rent-1.png"},
-                        new CarImage {Id = 2, ImageUrl = "car-rent-2.png", IsMain = true},
-                        new CarImage {Id = 3, ImageUrl = "car-rent-3.png"},
-                        new CarImage {Id = 3, ImageUrl = "car-rent-4.png"},
-                    }
-                },
-                new Car {
-                    Id = 2,
-                    Name ="Mercedes AMG GT",
-                    Price = 299,
-                    GearBox = "Automatic",
-                    Mileage = 22,
-                    Year = 2019,
-                    CarImages = new List<CarImage> {
-                        new CarImage {Id = 1, ImageUrl = "car-rent-1.png", IsMain = true},
-                        new CarImage {Id = 2, ImageUrl = "car-rent-2.png"},
-                        new CarImage {Id = 3, ImageUrl = "car-rent-3.png"},
-                        new CarImage {Id = 3, ImageUrl = "car-rent-4.png"},
-                    }
-                },
-                new Car {
-                    Id = 3,
-                    Name ="Audi R8",
-                    Price = 149,
-                    GearBox = "Manual",
-                    Mileage = 34,
-                    Year = 2012,
-                    CarImages = new List<CarImage> {
-                        new CarImage {Id = 1, ImageUrl = "car-rent-1.png"},
-                        new CarImage {Id = 2, ImageUrl = "car-rent-2.png"},
-                        new CarImage {Id = 3, ImageUrl = "car-rent-3.png", IsMain = true},
-                        new CarImage {Id = 3, ImageUrl = "car-rent-4.png"},
-                    }
-                },
-            };
         }
 
         public IActionResult Index()
         {
-            return View(Cars);
+            List<CarImage> carImg = _context.CarImages.ToList();
+            List<Car> cars = _context.Cars.ToList();
+            return View(cars);
         }
 
         public IActionResult Detail(int? id)
         {
+            List<CarImage> carImg = _context.CarImages.ToList();
+            List<Car> cars = _context.Cars.ToList();
             if (id is null) return BadRequest();
-            var carDetail = Cars.Where(c => c.Id == id).FirstOrDefault();
+            var carDetail = cars.Where(c => c.Id == id).FirstOrDefault();
             return View(carDetail);
         }
     }
---
+++ RentACar/Migrations/20220914002842_CreatedRentACarTable.cs
@@ -0,0 +1,63 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace RentACar.Migrations
+{
+    public partial class CreatedRentACarTable : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Cars",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Name = table.Column<string>(nullable: true),
+                    Description = table.Column<string>(nullable: true),
+                    Price = table.Column<decimal>(nullable: false),
+                    Mileage = table.Column<int>(nullable: false),
+                    Year = table.Column<int>(nullable: false),
+                    GearBox = table.Column<string>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Cars", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "CarImages",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    IsMain = table.Column<bool>(nullable: false),
+                    ImageUrl = table.Column<string>(nullable: true),
+                    CarId = table.Column<int>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_CarImages", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_CarImages_Cars_CarId",
+                        column: x => x.CarId,
+                        principalTable: "Cars",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Restrict);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_CarImages_CarId",
+                table: "CarImages",
+                column: "CarId");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "CarImages");
+
+            migrationBuilder.DropTable(
+                name: "Cars");
+        }
+    }
+}
